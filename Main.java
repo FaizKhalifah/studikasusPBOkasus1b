45338b7aa8ff88c4380b435c525f6847a13963af
FaizKhalifah <118319595+FaizKhalifah@users.noreply.github.com>
2023-03-07 23:23:14 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-    Hewan hewan1 = new Hewan();
-    hewan1.setNamaHewan("Linggau");
-    hewan1.setNoPelanggan("24543535345");
-    hewan1.setWarnaHewan("Cokelat");
-    hewan1.setTahunKelahiran("2018");
-    hewan1.setJenisHewan("Kucing");
-    hewan1.setLastTreatment(20);
-    hewan1.cetakInfo();
-        System.out.println();
-    hewan1.cekKesehatan();
-        System.out.println();
-    hewan1.treatmentKhusus();
-        System.out.println();
-    hewan1.cetakInfo();
-        System.out.println();
-
-    Hewan hewan2 = new Hewan("Bodas","2342433242","putih","2020","kelinci",11);
-    hewan2.cetakInfo();
-        System.out.println();
-    hewan2.cekKesehatan();
-        System.out.println();
-    hewan2.treatmentKhusus();
-        System.out.println();
-    hewan2.treatmentBiasa();
-    hewan2.cetakInfo();
+    Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        Hewan [] kumpulanHewan = new Hewan[a];
+        input.nextLine();
+        for (int i =0; i<a;i++ ){
+            String nama = input.nextLine();
+            String id = input.nextLine();
+            String warna = input.nextLine();
+            String tahun = input.nextLine();
+            String jenis = input.nextLine();
+            int lastTreatment = input.nextInt();
+            input.nextLine();
+            kumpulanHewan[i] = new Hewan(nama,id,warna,tahun,jenis,lastTreatment);
+            kumpulanHewan[i].cetakInfo();
+            kumpulanHewan[i].kasihMakan();
+        }
     }
 }
